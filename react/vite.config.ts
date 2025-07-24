61276bd9ca2b40ce8c49305abbb3332893c02ea3
Mickey Mullin <mickey@mullinator.org>
2025-07-24 18:26:25 -0400

    Corrected React path for Vite

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -4,17 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react/',
+  base: '/x-thread/',
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "src/styles/variables.scss";`
-      }
-    }
+        additionalData: `@import "src/styles/variables.scss";`,
+      },
+    ,}
   },
   resolve: {
     alias: {
-      '@': '/src'
-    }
-  }
+      '@': './src',
+    },
+  },
 })
